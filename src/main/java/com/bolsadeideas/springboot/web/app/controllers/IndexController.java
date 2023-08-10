82fd838ee056ea6d3ba8527cc4ca8233d0c89f25
AngelTravieso <angeltraviesoc@gmail.com>
2023-08-09 20:56:35 -0400

    Anotacion RequestMapping sobre el controlador

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -3,12 +3,14 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app") // => ruta de primer nivel, genérica para el controlador
 public class IndexController {
 
     // @RequestMapping(value = "/index", method = RequestMethod.GET) // => por defecto es get
-    @GetMapping({"/index", "/", "/home"}) // => mapeado a varias rutas
+    @GetMapping({"/index", "/", "/home", ""}) // => mapeado a varias rutas
     // Model model => model.addAttribute("key", "value");
     // ModelMap model
     // Map<String, Object> map => map.put("key", "value");
