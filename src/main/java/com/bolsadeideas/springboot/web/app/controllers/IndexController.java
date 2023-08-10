bf637898127aee1a547389b7979c8a3a2688a6bb
AngelTravieso <angeltraviesoc@gmail.com>
2023-08-09 20:52:56 -0400

    Pasar datos a la vista

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,16 +1,20 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
     // @RequestMapping(value = "/index", method = RequestMethod.GET) // => por defecto es get
     @GetMapping({"/index", "/", "/home"}) // => mapeado a varias rutas
-    public String index() {
+    // Model model => model.addAttribute("key", "value");
+    // ModelMap model
+    // Map<String, Object> map => map.put("key", "value");
+    // ModelAndView mv => mv.addObject("key", "value") => return mv;
+    public String index(Model model) {
+        model.addAttribute("titulo", "hola spring framework");
         return "index";
     }
 
